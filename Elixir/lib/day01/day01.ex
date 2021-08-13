98ea904ff52fd7fcfb4f72bb2e2a57ed02ac727c
VadAbel <22178832+VadAbel@users.noreply.github.com>
2021-08-13 18:39:30 +0200

    Add Day01

--- Elixir/lib/day01/day01.ex
+++ Elixir/lib/day01/day01.ex
@@ -2,28 +2,45 @@ defmodule Aoc2017.Day01 do
   @day "01"
   @input_file "../inputs/day#{@day}.txt"
 
-  def solution1(input) do
+  defp parser(input) do
     input
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp return_same([x, x]), do: x
+  defp return_same([_x, _y]), do: 0
+
+  def solution1(input) do
+    captcha = parser(input)
+
+    captcha
+    |> Stream.chunk_every(2, 1, [hd(captcha)])
+    |> Enum.reduce(0, &(return_same(&1) + &2))
   end
 
   def solution2(input) do
-    input
+    captcha = parser(input)
+
+    captcha
+    |> Stream.chunk_every(div(length(captcha), 2))
+    |> Enum.zip_reduce(0, &(return_same(&1) * 2 + &2))
   end
 
-  # @doc """
-  # iex> Aoc2017.Day01.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day01.part1()
+  1341
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day01.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day01.part2()
+  1348
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day01/day01_test.exs
+++ Elixir/test/day01/day01_test.exs
@@ -1,6 +1,6 @@
 defmodule Aoc2017Test.Day01Test do
   use ExUnit.Case
-  # import Aoc2017.Day01
+  import Aoc2017.Day01
 
   doctest Aoc2017.Day01
 
@@ -8,10 +8,17 @@ defmodule Aoc2017Test.Day01Test do
   # """
 
   test "Day01 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1("1122") == 3
+    assert solution1("1111") == 4
+    assert solution1("1234") == 0
+    assert solution1("91212129") == 9
   end
 
   test "Day01 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2("1212") == 6
+    assert solution2("1221") == 0
+    assert solution2("123425") == 4
+    assert solution2("123123") == 12
+    assert solution2("12131415") == 4
   end
 end
