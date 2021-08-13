98c71b98b9929bb42a0c97097d1211c03afc685c
VadAbel <22178832+VadAbel@users.noreply.github.com>
2021-08-13 19:20:05 +0200

    Add Day02

--- Elixir/lib/day02/day02.ex
+++ Elixir/lib/day02/day02.ex
@@ -2,28 +2,54 @@ defmodule Aoc2017.Day02 do
   @day "02"
   @input_file "../inputs/day#{@day}.txt"
 
+  defp parser(line) do
+    line
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+
   def solution1(input) do
     input
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn line, acc ->
+      {mini, maxi} =
+        line
+        |> parser()
+        |> Enum.min_max()
+
+      maxi - mini + acc
+    end)
   end
 
   def solution2(input) do
     input
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn
+      line, acc ->
+        row = parser(line)
+
+        for x <- row,
+            y <- row -- [x],
+            rem(x, y) == 0,
+            reduce: acc,
+            do: (acc -> div(x, y) + acc)
+    end)
   end
 
-  # @doc """
-  # iex> Aoc2017.Day02.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day02.part1()
+  44887
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day02.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day02.part2()
+  242
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day02/day02_test.exs
+++ Elixir/test/day02/day02_test.exs
@@ -1,17 +1,25 @@
 defmodule Aoc2017Test.Day02Test do
   use ExUnit.Case
-  # import Aoc2017.Day02
+  import Aoc2017.Day02
 
   doctest Aoc2017.Day02
 
-  # @test_input """
-  # """
+  @test_input """
+  5 1 9 5
+  7 5 3
+  2 4 6 8
+  """
+  @test_input_2 """
+  5 9 2 8
+  9 4 7 3
+  3 8 6 5
+  """
 
   test "Day02 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input) == 18
   end
 
   test "Day02 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2(@test_input_2) == 9
   end
 end
