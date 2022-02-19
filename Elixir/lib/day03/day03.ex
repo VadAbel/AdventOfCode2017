3b5c5994da59d9863f0c7fbf5f536fcd65c6871c
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-02-19 14:20:19 +0100

    Add Day03

--- Elixir/lib/day03/day03.ex
+++ Elixir/lib/day03/day03.ex
@@ -2,28 +2,99 @@ defmodule Aoc2017.Day03 do
   @day "03"
   @input_file "../inputs/day#{@day}.txt"
 
+  @start_position {0, 0}
+  @start_direction {1, 0}
+  @start_value 1
+
+  defp turn({x, y}), do: {-y, x}
+
+  defp next_direction(direction, {position_x, position_y})
+       when abs(position_x) == abs(position_y) and not (position_x >= 0 and position_y <= 0),
+       do: turn(direction)
+
+  defp next_direction(direction, {position_x, position_y})
+       when position_x == -position_y + 1 and (position_x >= 0 and position_y <= 0),
+       do: turn(direction)
+
+  defp next_direction(direction, {_position_x, _position_y}), do: direction
+
+  defp next_position({position_x, position_y}, {direction_x, direction_y}),
+    do: {position_x + direction_x, position_y + direction_y}
+
+  defp neigh({x, y}) do
+    [
+      {x - 1, y + 1},
+      {x - 1, y},
+      {x - 1, y - 1},
+      {x, y + 1},
+      {x, y - 1},
+      {x + 1, y - 1},
+      {x + 1, y},
+      {x + 1, y + 1}
+    ]
+  end
+
+  defp next({position, direction}) do
+    new_position = next_position(position, direction)
+    new_direction = next_direction(direction, new_position)
+
+    {new_position, new_direction}
+  end
+
+  defp next({_value, position, direction, position_map}) do
+    new_position = next_position(position, direction)
+    new_direction = next_direction(direction, new_position)
+
+    new_value =
+      position_map
+      |> Map.take(neigh(new_position))
+      |> Map.values()
+      |> Enum.sum()
+
+    {new_value, new_position, new_direction, Map.put(position_map, new_position, new_value)}
+  end
+
+  defp distance({x1, y1}, {x2, y2}),
+    do: abs(x1 - x2) + abs(y1 - y2)
+
   def solution1(input) do
-    input
+    mark =
+      input
+      |> String.to_integer()
+
+    {@start_position, @start_direction}
+    |> Stream.iterate(&next/1)
+    |> Enum.at(mark - 1)
+    |> elem(0)
+    |> distance(@start_position)
   end
 
   def solution2(input) do
-    input
+    mark =
+      input
+      |> String.to_integer()
+
+    {@start_value, @start_position, @start_direction, %{@start_position => @start_value}}
+    |> Stream.iterate(&next/1)
+    |> Stream.drop_while(fn {value, _, _, _} -> value < mark end)
+    |> Enum.at(0)
+    |> elem(0)
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  480
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  349975
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day03/day03_test.exs
+++ Elixir/test/day03/day03_test.exs
@@ -1,6 +1,6 @@
 defmodule Aoc2017Test.Day03Test do
   use ExUnit.Case
-  # import Aoc2017.Day03
+  import Aoc2017.Day03
 
   doctest Aoc2017.Day03
 
@@ -8,7 +8,10 @@ defmodule Aoc2017Test.Day03Test do
   # """
 
   test "Day03 Test1" do
-    # assert solution1(@test_inpout) == nil
+    assert solution1("1") == 0
+    assert solution1("12") == 3
+    assert solution1("23") == 2
+    assert solution1("1024") == 31
   end
 
   test "Day03 Test2" do
