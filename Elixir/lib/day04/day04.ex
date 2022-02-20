c3b9ee5264022111bf3b92675af6e6d062df2a4f
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-02-20 09:53:50 +0100

    Add Day04

--- Elixir/lib/day04/day04.ex
+++ Elixir/lib/day04/day04.ex
@@ -4,26 +4,43 @@ defmodule Aoc2017.Day04 do
 
   def solution1(input) do
     input
+    |> String.split("\n")
+    |> Enum.map(&String.split/1)
+    |> Enum.filter(&(&1 == Enum.uniq(&1)))
+    |> Enum.count()
   end
 
   def solution2(input) do
     input
+    |> String.split("\n")
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn password ->
+      for first <- password,
+          second <- password -- [first],
+          first_split = String.codepoints(first),
+          second_split = String.codepoints(second),
+          first_split -- second_split == [],
+          second_split -- first_split == [],
+          do: false
+    end)
+    |> Enum.filter(&(&1 == []))
+    |> Enum.count()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  386
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  208
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day04/day04_test.exs
+++ Elixir/test/day04/day04_test.exs
@@ -1,6 +1,6 @@
 defmodule Aoc2017Test.Day04Test do
   use ExUnit.Case
-  # import Aoc2017.Day04
+  import Aoc2017.Day04
 
   doctest Aoc2017.Day04
 
@@ -8,10 +8,16 @@ defmodule Aoc2017Test.Day04Test do
   # """
 
   test "Day04 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1("aa bb cc dd ee") == 1
+    assert solution1("aa bb cc dd aa") == 0
+    assert solution1("aa bb cc dd aaa") == 1
   end
 
   test "Day04 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2("abcde fghij") == 1
+    assert solution2("abcde xyz ecdab") == 0
+    assert solution2("a ab abc abd abf abj") == 1
+    assert solution2("iiii oiii ooii oooi oooo") == 1
+    assert solution2("oiii ioii iioi iiio") == 0
   end
 end
