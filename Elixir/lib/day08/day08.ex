2a882b2fcaa37ced5c18de2a7ae9f0c4f6aabccd
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-02-27 10:30:22 +0100

    Add Day08

--- Elixir/lib/day08/day08.ex
+++ Elixir/lib/day08/day08.ex
@@ -2,28 +2,89 @@ defmodule Aoc2017.Day08 do
   @day "08"
   @input_file "../inputs/day#{@day}.txt"
 
+  @start_register %{}
+
+  defp parse(line) do
+    [
+      instruction_register,
+      instruction_operator,
+      instruction_value,
+      _,
+      test_register,
+      test_operator,
+      test_value
+    ] =
+      line
+      |> String.split()
+
+    {
+      {instruction_register, instruction_operator, instruction_value |> String.to_integer()},
+      {test_register, test_operator, test_value |> String.to_integer()}
+    }
+  end
+
+  defp test?({r, o, v}, register) do
+    case o do
+      ">=" -> Map.get(register, r, 0) >= v
+      ">" -> Map.get(register, r, 0) > v
+      "<" -> Map.get(register, r, 0) < v
+      "<=" -> Map.get(register, r, 0) <= v
+      "==" -> Map.get(register, r, 0) == v
+      "!=" -> Map.get(register, r, 0) != v
+    end
+  end
+
+  defp exec({r, o, v}, register) do
+    case o do
+      "inc" -> Map.update(register, r, v, &(&1 + v))
+      "dec" -> Map.update(register, r, -v, &(&1 - v))
+    end
+  end
+
+  defp check_max(register, {r, _o, _v}, nil), do: {register, register[r]}
+  defp check_max(register, {r, _o, _v}, max_value), do: {register, max(max_value, register[r])}
+
   def solution1(input) do
     input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse(&1))
+    |> Enum.reduce(@start_register, fn {instruction, test}, register ->
+      case test?(test, register) do
+        true -> exec(instruction, register)
+        _ -> register
+      end
+    end)
+    |> Map.values()
+    |> Enum.max()
   end
 
   def solution2(input) do
     input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse(&1))
+    |> Enum.reduce({@start_register, nil}, fn {instruction, test}, {register, max_value} ->
+      case test?(test, register) do
+        true -> exec(instruction, register) |> check_max(instruction, max_value)
+        _ -> {register, max_value}
+      end
+    end)
+    |> elem(1)
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  3089
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  5391
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day08/day08_test.exs
+++ Elixir/test/day08/day08_test.exs
@@ -1,17 +1,22 @@
 defmodule Aoc2017Test.Day08Test do
   use ExUnit.Case
-  # import Aoc2017.Day08
+  import Aoc2017.Day08
 
   doctest Aoc2017.Day08
 
-  # @test_input """
-  # """
+  @test_input """
+  b inc 5 if a > 1
+  a inc 1 if b < 5
+  c dec -10 if a >= 1
+  c inc -20 if c == 10
+
+  """
 
   test "Day08 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input) == 1
   end
 
   test "Day08 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2(@test_input) == 10
   end
 end
