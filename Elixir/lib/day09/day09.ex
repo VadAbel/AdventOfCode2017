fe6ea4916c6c0b8dfdf6bb47eaaf93c8bdec4376
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-05-10 19:26:46 +0200

    Add Day09

--- Elixir/lib/day09/day09.ex
+++ Elixir/lib/day09/day09.ex
@@ -2,28 +2,73 @@ defmodule Aoc2017.Day09 do
   @day "09"
   @input_file "../inputs/day#{@day}.txt"
 
+  def group_count(text, group_level \\ 0, garbage_state \\ false)
+
+  def group_count("", _group_level, _garbage_state), do: 0
+
+  def group_count(<<?!, _b::bytes-size(1), rest::binary>>, group_level, garbage_state),
+    do: group_count(rest, group_level, garbage_state)
+
+  def group_count(<<?<, rest::binary>>, group_level, _garbage_state),
+    do: group_count(rest, group_level, true)
+
+  def group_count(<<?>, rest::binary>>, group_level, _garbage_state),
+    do: group_count(rest, group_level, false)
+
+  def group_count(<<?{, rest::binary>>, group_level, garbage_state) when garbage_state == false,
+    do: group_count(rest, group_level + 1, garbage_state)
+
+  def group_count(<<?}, rest::binary>>, group_level, garbage_state)
+      when garbage_state == false and group_level > 0,
+      do: group_level + group_count(rest, group_level - 1, garbage_state)
+
+  def group_count(<<_a::bytes-size(1), rest::binary>>, group_level, garbage_state),
+    do: group_count(rest, group_level, garbage_state)
+
+  def garbage_count(text, garbage_state \\ false)
+  def garbage_count("", _garbage_state), do: 0
+
+  def garbage_count(<<?!, _b::bytes-size(1), rest::binary>>, garbage_state),
+    do: garbage_count(rest, garbage_state)
+
+  def garbage_count(<<?<, rest::binary>>, garbage_state) when garbage_state == false,
+    do: garbage_count(rest, true)
+
+  def garbage_count(<<?>, rest::binary>>, garbage_state) when garbage_state == true,
+    do: garbage_count(rest, false)
+
+  def garbage_count(<<_a::bytes-size(1), rest::binary>>, garbage_state)
+      when garbage_state == true,
+      do: 1 + garbage_count(rest, garbage_state)
+
+  def garbage_count(<<_a::bytes-size(1), rest::binary>>, garbage_state)
+      when garbage_state == false,
+      do: garbage_count(rest, garbage_state)
+
   def solution1(input) do
     input
+    |> group_count()
   end
 
   def solution2(input) do
     input
+    |> garbage_count()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  11846
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  6285
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day09/day09_test.exs
+++ Elixir/test/day09/day09_test.exs
@@ -1,6 +1,6 @@
 defmodule Aoc2017Test.Day09Test do
   use ExUnit.Case
-  # import Aoc2017.Day09
+  import Aoc2017.Day09
 
   doctest Aoc2017.Day09
 
@@ -8,10 +8,23 @@ defmodule Aoc2017Test.Day09Test do
   # """
 
   test "Day09 Test1" do
-    # assert Aoc2017.Day09.solution1(@test_input) == nil
+    assert solution1("{}") == 1
+    assert solution1("{{{}}}") == 6
+    assert solution1("{{},{}}") == 5
+    assert solution1("{{{},{},{{}}}}") == 16
+    assert solution1("{<a>,<a>,<a>,<a>}") == 1
+    assert solution1("{{<ab>},{<ab>},{<ab>},{<ab>}}") == 9
+    assert solution1("{{<!!>},{<!!>},{<!!>},{<!!>}}") == 9
+    assert solution1("{{<a!>},{<a!>},{<a!>},{<ab>}}") == 3
   end
 
   test "Day08 Test2" do
-    # assert Aoc2017.Day09.solution2(@test_input) == nil
+    assert solution2("<>") == 0
+    assert solution2("<azertyuiopqsdf>") == 14
+    assert solution2("<<<<>") == 3
+    assert solution2("<{!>}>") == 2
+    assert solution2("<!!>") == 0
+    assert solution2("<!!!>>") == 0
+    assert solution2("<{o\"i!a,<{i<a>") == 10
   end
 end
