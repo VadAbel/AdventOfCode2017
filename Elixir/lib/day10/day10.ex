30a6e6c0f11b7e9ef281b1d8d35041fca368be7f
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-06-24 07:45:46 +0200

    Add Day10

--- Elixir/lib/day10/day10.ex
+++ Elixir/lib/day10/day10.ex
@@ -2,28 +2,88 @@ defmodule Aoc2017.Day10 do
   @day "10"
   @input_file "../inputs/day#{@day}.txt"
 
-  def solution1(input) do
+  @circle_size 256
+  @salt [17, 31, 73, 47, 23]
+  @knot_round 64
+  @chunk_size 16
+
+  use Bitwise
+
+  def parser(input) do
     input
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def parser2(input) do
+    input
+    |> String.to_charlist()
+    |> Enum.concat(@salt)
+  end
+
+  def knot(sequence, elements, time) do
+    {elements, position, _skip_size} =
+      Enum.reduce(1..time, {elements, 0, 0}, fn _x, acc -> knot(sequence, acc) end)
+
+    {a, b} = Enum.split(elements, -position)
+    b ++ a
+  end
+
+  def knot([], {elements, position, skip_size}), do: {elements, position, skip_size}
+
+  def knot([reverse_lenght | reverse_rest], {elements, position, skip_size}) do
+    {a, b} = Enum.split(elements, reverse_lenght)
+    {c, d} = Enum.split(b ++ Enum.reverse(a), skip_size)
+    new_elements = d ++ c
+
+    knot(
+      reverse_rest,
+      {
+        new_elements,
+        rem(position + reverse_lenght + skip_size, length(new_elements)),
+        rem(skip_size + 1, length(new_elements))
+      }
+    )
+  end
+
+  def solution1(input, size \\ @circle_size) do
+    input
+    |> parser()
+    |> knot(Enum.to_list(0..(size - 1)), 1)
+    |> Enum.take(2)
+    |> Enum.product()
   end
 
   def solution2(input) do
     input
+    |> parser2()
+    |> knot(Enum.to_list(0..(@circle_size - 1)), @knot_round)
+    |> Enum.chunk_every(@chunk_size)
+    |> Enum.map(fn list ->
+      Enum.reduce(list, fn x, acc ->
+        bxor(acc, x)
+      end)
+      |> Integer.to_string(16)
+      |> String.downcase()
+      |> String.pad_leading(2, "0")
+    end)
+    |> Enum.join()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  6909
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  "9d5f4561367d379cfbf04f8c471c0095"
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day10/day10_test.exs
+++ Elixir/test/day10/day10_test.exs
@@ -1,17 +1,19 @@
 defmodule Aoc2017Test.Day10Test do
   use ExUnit.Case
-  # import Aoc2017.Day10
+  import Aoc2017.Day10
 
   doctest Aoc2017.Day10
 
-  # @test_input """
-  # """
+  @test_input "3,4,1,5"
 
   test "Day10 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input, 5) == 12
   end
 
   test "Day10 Test2" do
-    # assert solution2(@test_input) == true
+    assert solution2("1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d"
+    assert solution2("") == "a2582a3a0e66e6e86e3812dcb672a272"
+    assert solution2("AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd"
+    assert solution2("1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e"
   end
 end
