e5cf4338e4871f4ed3bd5225bf6f9eb6e3dc7a59
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-08-13 09:02:51 +0200

    Add Day11

--- Elixir/lib/day11/day11.ex
+++ Elixir/lib/day11/day11.ex
@@ -2,28 +2,57 @@ defmodule Aoc2017.Day11 do
   @day "11"
   @input_file "../inputs/day#{@day}.txt"
 
+  @direction_step %{
+    "n" => {0, 2},
+    "ne" => {1, 1},
+    "se" => {1, -1},
+    "s" => {0, -2},
+    "sw" => {-1, -1},
+    "nw" => {-1, 1}
+  }
+
+  def parser(input) do
+    String.split(input, ",", trim: true)
+  end
+
+  def step(dir, {x, y}) do
+    {x_step, y_step} = @direction_step[dir]
+    {x + x_step, y + y_step}
+  end
+
+  def calc_distance({x, y}) do
+    div(abs(x) + abs(y), 2)
+  end
+
   def solution1(input) do
     input
+    |> parser()
+    |> Enum.reduce({0, 0}, &step(&1, &2))
+    |> calc_distance()
   end
 
   def solution2(input) do
     input
+    |> parser()
+    |> Enum.scan({0, 0}, &step(&1, &2))
+    |> Enum.map(&calc_distance(&1))
+    |> Enum.max()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  764
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  1532
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day11/day11_test.exs
+++ Elixir/test/day11/day11_test.exs
@@ -1,6 +1,6 @@
 defmodule Aoc2017Test.Day11Test do
   use ExUnit.Case
-  # import Aoc2017.Day11
+  import Aoc2017.Day11
 
   doctest Aoc2017.Day11
 
@@ -8,7 +8,10 @@ defmodule Aoc2017Test.Day11Test do
   # """
 
   test "Day11 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1("ne,ne,ne") == 3
+    assert solution1("ne,ne,sw,sw") == 0
+    assert solution1("ne,ne,s,s") == 2
+    assert solution1("se,sw,se,sw,sw") == 3
   end
 
   test "Day11 Test2" do
