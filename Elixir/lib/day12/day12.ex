1ff8f4364a5d79d38a69e195b8729b9b81dc460f
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-08-13 13:45:15 +0200

    Add Day12

--- Elixir/lib/day12/day12.ex
+++ Elixir/lib/day12/day12.ex
@@ -2,28 +2,66 @@ defmodule Aoc2017.Day12 do
   @day "12"
   @input_file "../inputs/day#{@day}.txt"
 
+  def parser(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn x ->
+      String.split(x, ["<->", ",", " "], trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Map.new(fn [a | b] -> {a, b} end)
+  end
+
+  def process(programs, [], processed),
+    do: {length(processed), programs}
+
+  def process(programs, [a | _] = to_process, processed) do
+    {list_programs, new_programs} = Map.pop(programs, a, [])
+    new_processed = [a | processed]
+
+    new_to_process = ((to_process ++ list_programs) |> Enum.uniq()) -- new_processed
+
+    process(new_programs, new_to_process, new_processed)
+  end
+
+  def process2(programs) when map_size(programs) == 0, do: 0
+
+  def process2(programs) do
+    a = programs |> Map.keys() |> hd()
+
+    1 +
+      (process(programs, [a], [])
+       |> elem(1)
+       |> process2())
+  end
+
   def solution1(input) do
     input
+    |> parser()
+    |> process([0], [])
+    |> elem(0)
   end
 
   def solution2(input) do
     input
+    |> parser()
+    |> process2()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  113
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  202
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day12/day12_test.exs
+++ Elixir/test/day12/day12_test.exs
@@ -1,17 +1,24 @@
 defmodule Aoc2017Test.Day12Test do
   use ExUnit.Case
-  # import Aoc2017.Day12
+  import Aoc2017.Day12
 
   doctest Aoc2017.Day12
 
-  # @test_input """
-  # """
+  @test_input """
+  0 <-> 2
+  1 <-> 1
+  2 <-> 0, 3, 4
+  3 <-> 2, 4
+  4 <-> 2, 3, 6
+  5 <-> 6
+  6 <-> 4, 5
+  """
 
   test "Day12 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input) == 6
   end
 
   test "Day12 Test2" do
-    # assert solution2() == nil
+    assert solution2(@test_input) == 2
   end
 end
