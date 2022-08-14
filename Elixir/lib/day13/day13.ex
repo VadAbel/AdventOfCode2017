a4ac2f8e2b9d2852536001b56cb1be6558c1321e
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-08-14 22:42:08 +0200

    Add Day13

--- Elixir/lib/day13/day13.ex
+++ Elixir/lib/day13/day13.ex
@@ -2,28 +2,56 @@ defmodule Aoc2017.Day13 do
   @day "13"
   @input_file "../inputs/day#{@day}.txt"
 
+  def parser(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn a ->
+      String.split(a, ": ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+  end
+
+  def calc_severity(firewall, delay \\ 0) do
+    firewall
+    |> Enum.filter(&depth_intercepted?(&1, delay))
+    |> Enum.map(&Tuple.product/1)
+    |> Enum.sum()
+  end
+
+  def intercepted?(firewall, delay), do: Enum.any?(firewall, &depth_intercepted?(&1, delay))
+
+  def depth_intercepted?({depth, range}, delay), do: rem(depth + delay, (range - 1) * 2) == 0
+
   def solution1(input) do
     input
+    |> parser()
+    |> calc_severity()
   end
 
   def solution2(input) do
-    input
+    firewall = input |> parser()
+
+    Stream.iterate(0, &(&1 + 1))
+    |> Stream.drop_while(&intercepted?(firewall, &1))
+    |> Enum.take(1)
+    |> hd()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  2160
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  3907470
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day13/day13_test.exs
+++ Elixir/test/day13/day13_test.exs
@@ -1,17 +1,21 @@
 defmodule Aoc2017Test.Day13Test do
   use ExUnit.Case
-  # import Aoc2017.Day13
+  import Aoc2017.Day13
 
   doctest Aoc2017.Day13
 
-  # @test_input """
-  # """
+  @test_input """
+  0: 3
+  1: 2
+  4: 4
+  6: 4
+  """
 
   test "Day13 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input) == 24
   end
 
   test "Day13 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2(@test_input) == 10
   end
 end
