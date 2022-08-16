83f62ba3c1bc3b4332044e7178a7605d48e57fc9
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-08-16 11:00:18 +0200

    Add Day16

--- Elixir/lib/day16/day16.ex
+++ Elixir/lib/day16/day16.ex
@@ -1,32 +1,98 @@
 defmodule Aoc2017.Day16 do
-  require Integer
-  use Bitwise, only_operators: true
-
   @day "16"
   @input_file "../inputs/day#{@day}.txt"
 
-  def solution1(input) do
+  def parser(input) do
     input
+    |> String.split(",", trim: true)
+    |> Enum.map(&(String.split_at(&1, 1) |> parse_instruction))
   end
 
-  def solution2(input) do
-    input
+  def parse_instruction({"s", operand}), do: {?s, String.to_integer(operand)}
+
+  def parse_instruction({"x", operand}),
+    do: {
+      ?x,
+      String.split(operand, "/", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.sort()
+    }
+
+  def parse_instruction({"p", operand}),
+    do: {
+      ?p,
+      String.split(operand, "/", trim: true)
+      |> Enum.map(&(String.to_charlist(&1) |> hd()))
+    }
+
+  def exec(programs, {?s, count}), do: Enum.slide(programs, -count..-1, 0)
+
+  def exec(programs, {?x, [idx1, idx2]}) do
+    {val2, programs2} = List.pop_at(programs, idx2)
+    {val1, programs1} = List.pop_at(programs2, idx1)
+
+    List.insert_at(programs1, idx1, val2)
+    |> List.insert_at(idx2, val1)
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  def exec(programs, {?p, [val1, val2]}),
+    do:
+      exec(
+        programs,
+        {?x,
+         [val1, val2]
+         |> Enum.map(&Enum.find_index(programs, fn x -> x == &1 end))
+         |> Enum.sort()}
+      )
+
+  def dance(instructions, programs),
+    do: Enum.reduce(instructions, programs, fn instruction, acc -> exec(acc, instruction) end)
+
+  def dance_loop(_instructions, programs, {time, limit_time}, _save) when time == limit_time,
+    do: programs
+
+  def dance_loop(_instructions, programs, {time, limit_time}, save)
+      when is_map_key(save, programs),
+      do:
+        Map.filter(save, fn {_key, value} -> value == rem(limit_time, time) end)
+        |> Map.keys()
+        |> hd()
+
+  def dance_loop(instructions, programs, {time, limit_time}, save),
+    do:
+      dance_loop(
+        instructions,
+        dance(instructions, programs),
+        {time + 1, limit_time},
+        Map.put(save, programs, time)
+      )
+
+  def solution1(input, programs \\ ?a..?p) do
+    input
+    |> parser()
+    |> dance(programs |> Enum.to_list())
+    |> List.to_string()
+  end
+
+  def solution2(input, programs \\ ?a..?p, time \\ 1_000) do
+    input
+    |> parser()
+    |> dance_loop(programs |> Enum.to_list(), {0, time}, %{})
+    |> List.to_string()
+  end
+
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  "namdgkbhifpceloj"
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  "ibmchklnofjpdeag"
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day16/day16_test.exs
+++ Elixir/test/day16/day16_test.exs
@@ -1,17 +1,16 @@
 defmodule Aoc2017Test.Day16Test do
   use ExUnit.Case
-  # import Aoc2017.Day16
+  import Aoc2017.Day16
 
   doctest Aoc2017.Day16
 
-  # @test_input """
-  # """
+  @test_input "s1,x3/4,pe/b"
 
   test "Day16 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input, ?a..?e) == "baedc"
   end
 
   test "Day16 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2(@test_input, ?a..?e, 2) == "ceadb"
   end
 end
