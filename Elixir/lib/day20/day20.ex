636515d4f2bc613dca7bda32506dbd5339c61ab8
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-11-20 21:29:22 +0100

    Add Day20

--- Elixir/lib/day20/day20.ex
+++ Elixir/lib/day20/day20.ex
@@ -2,28 +2,123 @@ defmodule Aoc2017.Day20 do
   @day "20"
   @input_file "../inputs/day#{@day}.txt"
 
+  def parser(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.with_index(fn item, idx -> {idx, parse_line(item)} end)
+    |> Map.new()
+  end
+
+  def parse_line(line) do
+    line
+    |> String.split(", ", trim: true)
+    |> Enum.map(fn item ->
+      [type, vector] = String.split(item, "=", trim: true)
+
+      {String.to_atom(type), parse_vector(vector)}
+    end)
+    |> Map.new()
+  end
+
+  def parse_vector(vector) do
+    vector
+    |> String.split(["<", ",", ">", " "], trim: true)
+    |> Enum.map(&String.to_integer(&1))
+  end
+
+  def manhattan(vector) do
+    vector
+    |> Enum.map(&abs/1)
+    |> Enum.sum()
+  end
+
+  def keep_min_by(particules, item) do
+    min_acceleration =
+      particules
+      |> Map.values()
+      |> Enum.map(fn particule ->
+        manhattan(particule[item])
+      end)
+      |> Enum.min()
+
+    Map.filter(particules, fn {_key, value} -> manhattan(value[item]) == min_acceleration end)
+  end
+
+  def vectors_add(vectors, item_1, item_2) do
+    Map.update!(
+      vectors,
+      item_1,
+      &Enum.zip_with(&1, vectors[item_2], fn a, b -> a + b end)
+    )
+  end
+
+  def simulate(particules) do
+    particules
+    |> Enum.map(fn {key, vectors} ->
+      {key,
+       vectors
+       |> vectors_add(:v, :a)
+       |> vectors_add(:p, :v)}
+    end)
+    |> Map.new()
+  end
+
+  def simulate_to_stab(particules, collision \\ false) do
+    new_particules =
+      if collision,
+        do: simulate(particules) |> detect_collision(),
+        else: simulate(particules)
+
+    if Enum.any?(new_particules, fn {key, vectors} ->
+         (manhattan(vectors[:v]) <= manhattan(particules[key][:v]) && manhattan(vectors[:a]) != 0) ||
+           manhattan(vectors[:p]) < manhattan(particules[key][:p])
+       end),
+       do: simulate_to_stab(new_particules, collision),
+       else: particules
+  end
+
+  def detect_collision(particules) do
+    positions =
+      Map.values(particules)
+      |> get_in([Access.all(), :p])
+
+    Map.filter(particules, fn {_key, vectors} ->
+      Enum.count(positions, fn p -> p == vectors[:p] end) == 1
+    end)
+  end
+
   def solution1(input) do
     input
+    |> parser()
+    |> keep_min_by(:a)
+    |> simulate_to_stab()
+    |> keep_min_by(:v)
+    |> keep_min_by(:p)
+    |> Map.keys()
+    |> hd()
   end
 
   def solution2(input) do
     input
+    |> parser()
+    |> simulate_to_stab(true)
+    |> map_size()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  119
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  471
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day20/day20_test.exs
+++ Elixir/test/day20/day20_test.exs
@@ -1,17 +1,25 @@
 defmodule Aoc2017Test.Day20Test do
   use ExUnit.Case
-  # import Aoc2017.Day20
+  import Aoc2017.Day20
 
   doctest Aoc2017.Day20
 
-  # @test_input """
-  # """
+  @test_input """
+  p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>
+  p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>
+  """
+  @test_input_2 """
+  p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>
+  p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>
+  p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>
+  p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>
+  """
 
   test "Day20 Test1" do
-    # assert Aoc2017.Day20.solution1(@test_input) == nil
+    assert solution1(@test_input) == 0
   end
 
   test "Day20 Test2" do
-    # assert Aoc2017.Day20.solution2(@test_input) == nil
+    assert solution2(@test_input_2) == 1
   end
 end
