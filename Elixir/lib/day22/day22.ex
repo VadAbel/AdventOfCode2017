1654aabf98bf265c1ad06541edbd45210b37432a
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-11-26 11:30:07 +0100

    Add Day22

--- Elixir/lib/day22/day22.ex
+++ Elixir/lib/day22/day22.ex
@@ -2,28 +2,88 @@ defmodule Aoc2017.Day22 do
   @day "22"
   @input_file "../inputs/day#{@day}.txt"
 
-  def solution1(input) do
-    input
+  @start_dir {0, -1}
+
+  def parser(input) do
+    start_nodes =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_charlist(&1))
+
+    start_pos =
+      [Enum.at(start_nodes, 0), start_nodes]
+      |> Enum.map(&(&1 |> length() |> div(2)))
+      |> List.to_tuple()
+
+    infected =
+      start_nodes
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {line, y} ->
+        Enum.with_index(line, fn
+          ?., x -> {{x, y}, :clean}
+          ?#, x -> {{x, y}, :infected}
+        end)
+      end)
+      |> Map.new()
+
+    {start_pos, @start_dir, infected}
   end
 
-  def solution2(input) do
-    input
+  def burst({{pos, dir, nodes}, infection}, evolve_fn) do
+    {state, new_nodes} =
+      get_and_update_in(nodes, [Access.key(pos, :clean)], &{&1, evolve_fn.(&1)})
+
+    new_infection =
+      if new_nodes[pos] == :infected,
+        do: infection + 1,
+        else: infection
+
+    new_dir = turn(dir, state)
+    new_pos = move(pos, new_dir)
+
+    {{new_pos, new_dir, new_nodes}, new_infection}
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  def turn({x, y}, :infected), do: {-y, x}
+  def turn({x, y}, :clean), do: {y, -x}
+  def turn({x, y}, :weakened), do: {x, y}
+  def turn({x, y}, :flagged), do: {-x, -y}
+
+  def move({pos_x, pos_y}, {dir_x, dir_y}), do: {pos_x + dir_x, pos_y + dir_y}
+  def evolve_1(:clean), do: :infected
+  def evolve_1(:infected), do: :clean
+
+  def evolve_2(:clean), do: :weakened
+  def evolve_2(:weakened), do: :infected
+  def evolve_2(:infected), do: :flagged
+  def evolve_2(:flagged), do: :clean
+
+  def solution1(input, burst \\ 10000) do
+    1..burst
+    |> Enum.reduce({parser(input), 0}, fn _, acc -> burst(acc, &evolve_1/1) end)
+    |> elem(1)
+  end
+
+  def solution2(input, burst \\ 10_000_000) do
+    1..burst
+    |> Enum.reduce({parser(input), 0}, fn _, acc -> burst(acc, &evolve_2/1) end)
+    |> elem(1)
+  end
+
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  5552
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  2511527
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day22/day22_test.exs
+++ Elixir/test/day22/day22_test.exs
@@ -1,17 +1,22 @@
 defmodule Aoc2017Test.Day22Test do
   use ExUnit.Case
-  # import Aoc2017.Day22
+  import Aoc2017.Day22
 
   doctest Aoc2017.Day22
 
-  # @test_input """
-  # """
+  @test_input """
+  ..#
+  #..
+  ...
+  """
 
   test "Day22 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input, 7) == 5
+    assert solution1(@test_input) == 5587
   end
 
   test "Day22 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2(@test_input, 100) == 26
+    assert solution2(@test_input) == 2_511_944
   end
 end
