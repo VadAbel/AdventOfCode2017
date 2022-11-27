e866a9ddffe971d5470641850389cd1cfb07577b
VadAbel <22178832+VadAbel@users.noreply.github.com>
2022-11-27 10:17:48 +0100

    Add Day24

--- Elixir/lib/day24/day24.ex
+++ Elixir/lib/day24/day24.ex
@@ -2,28 +2,72 @@ defmodule Aoc2017.Day24 do
   @day "24"
   @input_file "../inputs/day#{@day}.txt"
 
+  @start_port 0
+
+  def parser(input) do
+    input
+    |> String.split(["\n"], trim: true)
+    |> Enum.map(fn line ->
+      String.split(line, ["/"], trim: true)
+      |> Enum.map(&String.to_integer(&1))
+    end)
+  end
+
+  def possible_components(components, port), do: Enum.filter(components, &(port in &1))
+
+  def make_bridge(components, port),
+    do: make_bridge(components, possible_components(components, port), port)
+
+  def make_bridge(_components, [], _port), do: [[]]
+
+  def make_bridge(components, possible, port) do
+    for component <- possible,
+        new_components = components -- [component],
+        new_port = (component -- [port]) |> hd(),
+        rest <- make_bridge(new_components, new_port),
+        do: [component | rest]
+  end
+
+  def longest_bridges(bridges) do
+    max_length =
+      bridges
+      |> Enum.map(&length(&1))
+      |> Enum.max()
+
+    Enum.filter(bridges, &(length(&1) == max_length))
+  end
+
   def solution1(input) do
     input
+    |> parser()
+    |> make_bridge(@start_port)
+    |> Enum.map(&(List.flatten(&1) |> Enum.sum()))
+    |> Enum.max()
   end
 
   def solution2(input) do
     input
+    |> parser()
+    |> make_bridge(@start_port)
+    |> longest_bridges()
+    |> Enum.map(&(List.flatten(&1) |> Enum.sum()))
+    |> Enum.max()
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part1()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part1()
+  1868
+  """
   def part1 do
     File.read!(@input_file)
     |> solution1
     |> IO.inspect(label: "Day#{@day} Part1 result ")
   end
 
-  # @doc """
-  # iex> Aoc2017.Day#{@day}.part2()
-  # nil
-  # """
+  @doc """
+  iex> Aoc2017.Day#{@day}.part2()
+  1841
+  """
   def part2 do
     File.read!(@input_file)
     |> solution2
--- Elixir/test/day24/day24_test.exs
+++ Elixir/test/day24/day24_test.exs
@@ -1,17 +1,25 @@
 defmodule Aoc2017Test.Day24Test do
   use ExUnit.Case
-  # import Aoc2017.Day24
+  import Aoc2017.Day24
 
   doctest Aoc2017.Day24
 
-  # @test_input """
-  # """
+  @test_input """
+  0/2
+  2/2
+  2/3
+  3/4
+  3/5
+  0/1
+  10/1
+  9/10
+  """
 
   test "Day24 Test1" do
-    # assert solution1(@test_input) == nil
+    assert solution1(@test_input) == 31
   end
 
   test "Day24 Test2" do
-    # assert solution2(@test_input) == nil
+    assert solution2(@test_input) == 19
   end
 end
